eabef2062355d555836c9dde88ef0e7bc0f03b38
guotian <987510914@qq.com>
2022-05-27 17:43:44 +0800

    dp2

--- 62.不同路径.js
+++ 62.不同路径.js
@@ -11,7 +11,26 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
+  let dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  dp[0].fill(1);
+  if(n === 1) {
+    return 1;
+  }
 
+  for(let i = 1;i < m;i++) {
+    for(let j = 1;j < n;j++) {
+      if(j === 1) {
+        dp[i][j-1] = 1;
+      }
+      dp[i][j] = dp[i-1][j] + dp[i][j-1];
+    }
+  }
+
+  return dp[m-1][n-1];
 };
 // @lc code=end
 
+
+// @after-stub-for-debug-begin
+module.exports = uniquePaths;
+// @after-stub-for-debug-end
